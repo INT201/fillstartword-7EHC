80ebb6c0946938558af25fe7499a1efd5aa53e89
Purin <purin.peera@mail.kmutt.ac.th>
2022-09-11 15:07:50 +0700

    update commit

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,12 @@
-const { template } = require('@babel/core')
+//const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
+    return startWord+word
 }
 module.exports = fillStartWord
+console.log(fillStartWord("1-2565-"+"Hello World")) 
+console.log(fillStartWord("๋JS"+"beginner"))
+console.log(fillStartWord("first"+"step"))
+console.log(fillStartWord("first"+null))
+console.log(fillStartWord("first"+undefined))
