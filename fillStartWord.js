c2bd1e25a89d2347b6ed316e775b1a4e16f437e7
Purin <purin.peera@mail.kmutt.ac.th>
2022-09-11 15:08:38 +0700

    update commit

--- fillStartWord.js
+++ fillStartWord.js
@@ -5,8 +5,4 @@ function fillStartWord(startWord, word) {
     return startWord+word
 }
 module.exports = fillStartWord
-console.log(fillStartWord("1-2565-"+"Hello World")) 
-console.log(fillStartWord("๋JS"+"beginner"))
-console.log(fillStartWord("first"+"step"))
-console.log(fillStartWord("first"+null))
-console.log(fillStartWord("first"+undefined))
+
